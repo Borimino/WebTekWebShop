d508af81cd87ec179d025df2a67fdcdf815062df
g74un3 <daniel@graungaard.com>
2014-02-21 15:36:44 +0100

    Merge branch 'master' of https://github.com/Borimino/WebTekWebShop.git
    Conflicts:
    	TomcatServer/src/TempTESTER.java

--- TomcatServer/src/PanelBean.java
+++ TomcatServer/src/PanelBean.java
@@ -55,10 +55,12 @@ public class PanelBean {
 		int id = Integer.parseInt(e.getChildText("itemID", nameSpace));
 
 		Element descriptionElement = e.getChild("itemDescription", nameSpace);
-
+		
+		String XMLdescription = XMLHandler.getOutputter().outputString(descriptionElement);
+		
 		Element eDescription = convertToHTML(descriptionElement.getChild("document", nameSpace));
 
-		String description = XMLHandler.getOutputter().outputString(eDescription);
+		String HTMLdescription = XMLHandler.getOutputter().outputString(eDescription);
 
 		String url = e.getChildText("itemURL", nameSpace);
 
@@ -73,7 +75,7 @@ public class PanelBean {
 
 		int price = Integer.parseInt(e.getChildText("itemPrice", nameSpace));
 
-		Item temp = new Item(name, id, description, url, stock, price);
+		Item temp = new Item(name, id, HTMLdescription, XMLdescription, url, stock, price);
 
 		target.add(temp);
 
