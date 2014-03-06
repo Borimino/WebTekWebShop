6a19a93c8624804bdb912b5497b2c9bf5187635b
g74un3 <daniel@graungaard.com>
2014-03-06 23:33:37 +0100

    Worked on basket

--- TomcatServer/WebContent/js/basket.js
+++ TomcatServer/WebContent/js/basket.js
@@ -2,21 +2,21 @@
  * HRELLOEW LITTLE BASKET!
  */
 
+function test() {
 
-function test(){
+	$.ajax({
+		type : "POST",
+		url : "rest/basket/update",
+		data : {
+			itemID : "3169",
+			amount : "10"
 
-$.ajax({
-	type : "POST",
-	url : "rest/basket/update",
-	data : { items : {
-		"3169": "10",
-		"3175": "15"
-	}},
-	success : function(data) {
+		},
+		success : function(data) {
 
-		alert(data);
+			alert(data);
 
-	}
-});
+		}
+	});
 
 }
--- TomcatServer/src/ShopBasket.java
+++ TomcatServer/src/ShopBasket.java
@@ -1,10 +1,15 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.Namespace;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -15,43 +20,65 @@ import javax.ws.rs.core.Context;
 @Path("basket")
 public class ShopBasket {
 
-    @Context HttpSession session;
+	HttpSession session;
 	private CloudHandler cloudHandler;
 	private Namespace n;
-	
-	
-	public ShopBasket() {
 
-		this(Namespace.getNamespace("http://www.cs.au.dk/dWebTek/2014"));
-	
+	public ShopBasket(@Context HttpServletRequest sessionrequest) {
+
+		this(Namespace.getNamespace("http://www.cs.au.dk/dWebTek/2014"),
+				sessionrequest);
+
 	}
-	
-	public ShopBasket(Namespace n) {
+
+	public ShopBasket(Namespace n, @Context HttpServletRequest sessionrequest) {
 		this.n = n;
-		cloudHandler = new CloudHandler(n);	
+		cloudHandler = new CloudHandler(n);
+		session = sessionrequest.getSession();
 	}
-	
+
 	@POST
 	@Path("update")
-	public void updateBasket(@FormParam("items") String items){
+	public String updateBasket(@FormParam("itemID") String itemID,
+			@FormParam("amount") String amount) {
+
+		Element root = new Element("sellItems");
+		Document buydoc = new Document(root);
+		
+		Element shopIDElement = new Element("shopKey");
+		shopIDElement.addContent("1E3D5BA1FD481ECFC983D4B3");
+		
+		Element itemIDElement = new Element("itemID");
+		itemIDElement.addContent(itemID);
+		
+		Element customerIDElement = new Element("customerID");
+		String customerID = (String) session.getAttribute("id");
+		customerIDElement.addContent(customerID);
+		
+		Element saleAmountElement = new Element("saleAmount");
+		saleAmountElement.addContent(amount);
+		
+		
+		root.addContent(shopIDElement);
+		root.addContent(itemIDElement);
+		root.addContent(customerIDElement);
+		root.addContent(saleAmountElement);
+		
+		
+		HttpURLConnection con = cloudHandler.connect(true, "/sellItems");
+		cloudHandler.getResponse(true, con, buydoc, XMLHandler.getOutputter(),
+				XMLHandler.getSAXBuilder());
+
+		try {
+			XMLHandler.getOutputter().output(buydoc, System.out);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "SUCCESSS!!!!!!!!!!!!";
 
 		
-		JSONObject res = new JSONObject(items);
-		
-		System.out.println(res);
-		
-		
-		//HVAD FANDEN ER DET???
-		
-		session.getAttribute("id");
-		
-		
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
