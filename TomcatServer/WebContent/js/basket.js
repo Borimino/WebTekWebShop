54b1b051461dcb05083ef4cc22e657621b0ceca8
g74un3 <daniel@graungaard.com>
2014-03-07 16:48:03 +0100

    Changed basket update

--- TomcatServer/WebContent/js/basket.js
+++ TomcatServer/WebContent/js/basket.js
@@ -7,62 +7,65 @@ var productlist = [];
 var item_count = 0;
 var total = 0;
 
-function add_to_basket(itemID, itemAmount, priceprItem, itemname){
+function add_to_basket(itemID, itemAmount, priceprItem, itemname) {
 
 	var res = {};
 
 	res["itemID"] = itemID;
 	res["amount"] = itemAmount;
 	basket.push(res);
-	
+
 	productlist.push(itemname);
-	
+
 	total += priceprItem * itemAmount;
 	item_count += itemAmount;
-	
+
 	basket_update();
 
 }
 
 function empty_basket() {
-	
-	
-	if(basket.length > 0){
-	$.ajax({
-		type : "POST",
-		url : "rest/basket/update",
-		data : {
-			itemList : JSON.stringify(basket)
 
-		},
-		success : function(answer) {
+	if (basket.length > 0) {
+		$.ajax({
+			type : "POST",
+			url : "rest/basket/update",
+			data : {
+				itemList : JSON.stringify(basket)
 
-			alert(answer);
-			total = 0;
-			basket = [];
-			item_count = 0;
-			basket_update();
-		}
-	});
+			},
+			success : function(answer) {
+
+				alert(answer);
+
+				if (answer != "Not logged in!") {
+
+					total = 0;
+					basket = [];
+					item_count = 0;
+				}
+				basket_update();
+			}
+		});
 
 	} else {
-		
+
 		alert("Du skal smidde et item i kurven inden du kan købe det!!!!!");
-		
+
 	}
-	
+
 }
 
-function basket_update(){
-	
+function basket_update() {
+
 	$('#basketlist').text("");
-	
-	$(productlist).each(function(index, item){
-		
+
+	$(productlist).each(function(index, item) {
+
 		$('#basketlist').append($(document.createElement("li")).text(item));
-		
+
 	});
-	
-	$('#baskettotal').html(total); 
-	
+
+	$('#baskettotal').html(total);
+
 }
