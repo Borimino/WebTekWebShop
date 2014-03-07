da030ecb77aa3ba585f0681ea7ed0b39cb9e4cee
g74un3 <daniel@graungaard.com>
2014-03-07 13:50:27 +0100

    cHANGED JAVASCRIPT BASKET AND ALSO A RANDOM PIECE OF PORK

--- TomcatServer/WebContent/js/basket.js
+++ TomcatServer/WebContent/js/basket.js
@@ -2,39 +2,28 @@
  * HRELLOEW LITTLE BASKET!
  */
 
-var basket = {};
+var basket = [];
 var item_count = 0;
 
 function add_to_basket(itemID, itemAmount) {
 
-	basket[itemID] = itemAmount;
+	var res = {};
+
+	res[itemID] = itemAmount;
+	basket.push(res);
 	item_count += itemAmount;
+	
+	
 }
+
 
 function empty_basket() {
-
-
-	function basket_loop() {
-
-		setTimeout(function() {
-
-			id =
-			buy_item(id, basket[id]);
-
-		}, delay);
-
-	}
-
-}
-
-function buy_item(itemID, itemAmount) {
 
 	$.ajax({
 		type : "POST",
 		url : "rest/basket/update",
 		data : {
-			itemID : itemID,
-			amount : itemAmount
+			itemList : basket
 
 		},
 		success : function(answer) {
